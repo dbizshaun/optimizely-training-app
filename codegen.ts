50d161a61954d6e8de0f06a46d0db29a66f151d8
Shaun Antony Mark Azhichkakath Freddie <sfreddie@aldar.com>
2025-12-16 12:46:37 +0530

    - added prettier, jest unit testing

--- codegen.ts
+++ codegen.ts
@@ -1,29 +1,36 @@
 // #region Environment variable file parsing
 // Environment file parsing and updating
-import * as DotEnv from 'dotenv'
-import { expand } from 'dotenv-expand'
-import path from 'node:path'
-import fs from 'node:fs'
-import figures from 'figures'
-import chalk from 'chalk'
+import * as DotEnv from "dotenv";
+import { expand } from "dotenv-expand";
+import path from "node:path";
+import fs from "node:fs";
+import figures from "figures";
+import chalk from "chalk";
+import { fragments } from "@remkoj/optimizely-graph-functions/documents";
 
 // Process environment files, to ensure the enviornment configuration is applied
-const envFiles : string[] = [".env", ".env.local"]
+const envFiles: string[] = [".env", ".env.local"];
 if (process.env.NODE_ENV) {
-    envFiles.push(`.env.${ process.env.NODE_ENV }`)
-    envFiles.push(`.env.${ process.env.NODE_ENV }.local`)
+    envFiles.push(`.env.${process.env.NODE_ENV}`);
+    envFiles.push(`.env.${process.env.NODE_ENV}.local`);
 }
-envFiles.map(s => path.join(process.cwd(), s)).filter(s => fs.existsSync(s)).reverse().forEach(fileName => {
-    var result = DotEnv.config({ path: fileName, override: false })
-    expand(result)
-    console.log(`${ chalk.greenBright(figures.tick) } Processed ${fileName}`)
-})
+envFiles
+    .map((s) => path.join(process.cwd(), s))
+    .filter((s) => fs.existsSync(s))
+    .reverse()
+    .forEach((fileName) => {
+        var result = DotEnv.config({ path: fileName, override: false });
+        expand(result);
+        console.log(`${chalk.greenBright(figures.tick)} Processed ${fileName}`);
+    });
 // #endregion
 
 // Actual code generation setup
-import type { CodegenConfig  } from '@graphql-codegen/cli'
-import getSchemaInfo from '@remkoj/optimizely-graph-client/codegen'
-import OptimizelyGraphPreset, {type PresetOptions as OptimizelyGraphPresetOptions}  from '@remkoj/optimizely-graph-functions/preset'
+import type { CodegenConfig } from "@graphql-codegen/cli";
+import getSchemaInfo from "@remkoj/optimizely-graph-client/codegen";
+import OptimizelyGraphPreset, {
+    type PresetOptions as OptimizelyGraphPresetOptions,
+} from "@remkoj/optimizely-graph-functions/preset";
 
 // Create the configuration itself
 const config: CodegenConfig = {
@@ -32,13 +39,13 @@ const config: CodegenConfig = {
     // Allow & parse GraphQL Queries from anywhere within the codebase
     documents: [
         // Add local GraphQL files
-        'src/**/*.graphql',
+        "src/**/*.graphql",
 
         // Add Definitions from components
-        'src/**/!(*.d).{ts,tsx}'
+        "src/**/!(*.d).{ts,tsx}",
     ],
     generates: {
-        './src/gql/': {
+        "./src/gql/": {
             preset: OptimizelyGraphPreset,
             presetConfig: {
                 // By default the preset will generate recursive queries
@@ -48,7 +55,7 @@ const config: CodegenConfig = {
                 recursion: true,
 
                 // The GQL tag to be used to identify inline GraphQL queries
-                gqlTagName: 'gql',
+                gqlTagName: "gql",
 
                 // Configure the fragments that will be spread into the utility
                 // partial fragments. You can use any fragment here, however the
@@ -60,48 +67,60 @@ const config: CodegenConfig = {
                     {
                         // Add from all Pages, within code
                         into: "PageData",
-                        pathRegex: "src\/components\/cms\/page\/.*\.[tj]s(x){0,1}$"
+                        pathRegex: "src/components/cms/page/.*.[tj]s(x){0,1}$",
                     },
                     {
                         // Add from all Experiences, within code
                         into: "PageData",
-                        pathRegex: "src\/components\/cms\/experience\/.*\.[tj]s(x){0,1}$"
+                        pathRegex: "src/components/cms/experience/.*.[tj]s(x){0,1}$",
                     },
                     {
                         // Add from all Blocks, within code
                         into: "BlockData",
-                        pathRegex: "src\/components\/cms\/component\/.*\.[tj]s(x){0,1}$"
+                        pathRegex: "src/components/cms/component/.*.[tj]s(x){0,1}$",
                     },
                     {
                         // Add from all Elements, within code
                         into: "ElementData",
-                        pathRegex: "src\/components\/cms\/element\/.*\.[tj]s(x){0,1}$"
+                        pathRegex: "src/components/cms/element/.*.[tj]s(x){0,1}$",
                     },
                     {
                         // Add from all Pages, as .page.graphql file
                         into: "PageData",
-                        pathRegex: "src\/components\/cms\/.*\.page\.graphql$"
+                        pathRegex: "src/components/cms/.*.page.graphql$",
                     },
                     {
                         // Add from all Experiences, as .experience.graphql file
                         into: "PageData",
-                        pathRegex: "src\/components\/cms\/.*\.experience\.graphql$"
+                        pathRegex: "src/components/cms/.*.experience.graphql$",
                     },
                     {
                         // Add from all Blocks, as .component.graphql file
                         into: "BlockData",
-                        pathRegex: "src\/components\/cms\/.*\.component\.graphql$"
+                        pathRegex: "src/components/cms/.*.component.graphql$",
                     },
                     {
                         // Add from all Elements, as .element.graphql file
                         into: "ElementData",
-                        pathRegex: "src\/components\/cms\/.*\.element\.graphql$"
-                    }
+                        pathRegex: "src/components/cms/.*.element.graphql$",
+                    },
                 ],
-            } as OptimizelyGraphPresetOptions
-        }
+            } as OptimizelyGraphPresetOptions,
+        },
+        "./schema.graphql": {
+            plugins: ["schema-ast"],
+            config: {
+                includeDirectives: true,
+                commentDescriptions: true,
+                sort: true,
+                skipDocumentsValidation: {
+                    ignoreRules: ["NoFragmentCyclesRule"],
+                },
+            },
+            documents: [...fragments, "!src/**/*.{ts,tsx}"],
+        },
     },
-    ignoreNoDocuments: false
-}
+    ignoreNoDocuments: false,
+};
 
-export default config
+export default config;
